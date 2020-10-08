be48158e4c2184333e8d164367c6ae9d41e97e32
Jakub Flasiński <jk.flasinski@gmail.com>
2020-10-08 15:51:43 +0200

    change validateUniqueness to accept operator

--- src/services/validations/validateUniqueness.js
+++ src/services/validations/validateUniqueness.js
@@ -1,8 +1,8 @@
 import { UnprocessableEntity } from '../../utils/errors';
 import { Op } from 'sequelize';
 
-const validateUniqueness = async (model, field, value, scope = {}) => {
-  const count = await model.count({ where: { [field]: { [Op.iLike]: value }, ...scope } })
+const validateUniqueness = async (model, field, value, scope = {}, operator = Op.iLike) => {
+  const count = await model.count({ where: { [field]: { [operator]: value }, ...scope } })
   if (count > 0) {
     throw new UnprocessableEntity({ [field]: `${field} must be unique` });
   }
