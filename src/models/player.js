edc78ff1d3e6e1490d57ca50056f85631198e71a
Jakub Flasiński <jk.flasinski@gmail.com>
2020-10-08 14:11:38 +0200

    add answer relationship to other models

--- src/models/player.js
+++ src/models/player.js
@@ -6,6 +6,7 @@ module.exports = (sequelize, DataTypes) => {
   class Player extends Model {
     static associate(models) {
       Player.belongsTo(models.GameSession, { foreignKey: 'gameSessionId', as: 'gameSession' });
+      Player.hasMany(models.Answer, { foreignKey: 'playerId' });
     }
   };
   Player.init({
--- src/models/roundelement.js
+++ src/models/roundelement.js
@@ -6,6 +6,7 @@ module.exports = (sequelize, DataTypes) => {
   class RoundElement extends Model {
     static associate(models) {
       RoundElement.belongsTo(models.GameSession, { foreignKey: 'gameSessionId', as: 'gameSession' });
+      RoundElement.hasMany(models.Answer, { foreignKey: 'roundElementId' });
     }
   };
   RoundElement.init({
