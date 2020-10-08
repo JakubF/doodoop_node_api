dccea29a014825b8c2a0fe4971e0b9cecb763976
Jakub Flasiński <jk.flasinski@gmail.com>
2020-10-08 11:56:03 +0200

    add game session update service

---
+++ src/services/gameSessions/update.js
@@ -0,0 +1,15 @@
+import { GameSession } from '../../models';
+import validateName from '../validations/validateName';
+import { NotFound } from '../../utils/errors';
+
+const service = async ({ id, name }) => {
+  const record = await GameSession.findOne({ where: { id } })
+  if (!record)
+    throw new NotFound(`Record with id ${id} not found.`)
+  if (name === record.name)
+    return record
+
+  return await record.update({ name, updatedAt: new Date() })
+};
+
+export default service;
