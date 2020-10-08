b5ccdab74650d67e54a786bf42b7340c40880b88
Jakub Flasiński <jk.flasinski@gmail.com>
2020-10-08 09:33:20 +0200

    add round elements model

---
+++ src/db/migrations/20201008065032-create-round-element.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('RoundElements', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      answer: {
+        type: Sequelize.STRING
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      points: {
+        type: Sequelize.INTEGER
+      },
+      gameSessionId: {
+        type: Sequelize.INTEGER,
+        references: { model: 'GameSessions', key: 'id' }
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('RoundElements');
+  }
+};
---
+++ src/models/roundelement.js
@@ -0,0 +1,22 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class RoundElement extends Model {
+    static associate(models) {
+      RoundElement.belongsTo(models.GameSession);
+    }
+  };
+  RoundElement.init({
+    name: DataTypes.STRING,
+    answer: DataTypes.STRING,
+    status: DataTypes.STRING,
+    points: DataTypes.INTEGER,
+    gameSessionId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'RoundElement',
+  });
+  return RoundElement;
+};
