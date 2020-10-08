f88c5e71d479afd30c8255f6ab182b8ca2a3a4b8
Jakub Flasiński <jk.flasinski@gmail.com>
2020-10-08 16:32:05 +0200

    add status filter to gameSession entity

--- src/entities/gameSession.js
+++ src/entities/gameSession.js
@@ -3,7 +3,7 @@ import { RoundElement, Answer } from '../models';
 
 const gameSession = async (record) => {
   const currentRoundElement = await RoundElement.findOne({
-    where: { gameSessionId: record.id },
+    where: { gameSessionId: record.id, status: { [Op.ne]: 'completed' } },
     order: [['id', 'ASC']],
     include: [{ model: Answer, required: false, as: 'answers' },]
   })
