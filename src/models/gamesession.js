c74a74bb7fe0137a061824b9c48daab095e26752
Jakub Flasiński <jk.flasinski@gmail.com>
2020-10-08 09:33:12 +0200

    add game sessions model

---
+++ src/models/gamesession.js
@@ -0,0 +1,21 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class GameSession extends Model {
+    static associate(models) {
+      GameSession.hasMany(models.RoundElement);
+    }
+  };
+  GameSession.init({
+    name: DataTypes.STRING,
+    enterCode: DataTypes.STRING,
+    status: DataTypes.ENUM('pending', 'in_progress', 'completed')
+  }, {
+    sequelize,
+    modelName: 'GameSession',
+  });
+  return GameSession;
+};
