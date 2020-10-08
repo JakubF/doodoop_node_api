82c41ad6fb30105db588d5e50154f1bf5a975cd4
Jakub Flasiński <jk.flasinski@gmail.com>
2020-10-08 15:51:55 +0200

    add relationship alias to RE

--- src/models/roundelement.js
+++ src/models/roundelement.js
@@ -6,13 +6,13 @@ module.exports = (sequelize, DataTypes) => {
   class RoundElement extends Model {
     static associate(models) {
       RoundElement.belongsTo(models.GameSession, { foreignKey: 'gameSessionId', as: 'gameSession' });
-      RoundElement.hasMany(models.Answer, { foreignKey: 'roundElementId' });
+      RoundElement.hasMany(models.Answer, { foreignKey: 'roundElementId', as: 'answers' });
     }
   };
   RoundElement.init({
     name: DataTypes.STRING,
     answer: DataTypes.STRING,
-    status: DataTypes.STRING,
+    status: DataTypes.ENUM('pending', 'started', 'playing', 'completed'),
     points: DataTypes.INTEGER,
     gameSessionId: DataTypes.INTEGER,
     link: DataTypes.STRING,
