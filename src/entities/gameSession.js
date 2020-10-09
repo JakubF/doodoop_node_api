8a73d06391738685da301d6538489a7578fc2e7e
Jakub Flasiński <jk.flasinski@gmail.com>
2020-10-09 08:32:52 +0200

    include missing relations for game session entity

--- src/entities/gameSession.js
+++ src/entities/gameSession.js
@@ -1,11 +1,27 @@
 import { Op } from 'sequelize';
-import { RoundElement, Answer } from '../models';
+import { RoundElement, Answer, Player } from '../models';
 
 const gameSession = async (record) => {
   const currentRoundElement = await RoundElement.findOne({
     where: { gameSessionId: record.id, status: { [Op.ne]: 'completed' } },
     order: [['id', 'ASC']],
-    include: [{ model: Answer, required: false, as: 'answers' },]
+    include: [{
+      model: Answer,
+      required: false,
+      as: 'answers',
+      include: [
+        {
+          model: RoundElement,
+          required: false,
+          as: 'roundElement'
+        },
+        {
+          model: Player,
+          required: false,
+          as: 'player'
+        },
+      ]
+    }],
   })
  return {
    currentRoundElement
