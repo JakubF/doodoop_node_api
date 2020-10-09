de99359603a7259e4bba1bb3f1b16479ca5967c6
Jakub Flasiński <jk.flasinski@gmail.com>
2020-10-09 10:01:48 +0200

    add includes to join

--- src/services/gameSessions/join.js
+++ src/services/gameSessions/join.js
@@ -3,6 +3,10 @@ import { NotFound, UnprocessableEntity } from '../../utils/errors';
 import gameSessionEntity from '../../entities/gameSession';
 import { entityWrapper } from '../../utils/entityWrapper';
 
+const includes = [
+  { model: GameSession, required: false, as: 'gameSession' },
+]
+
 const findGameSession = async (enterCode) => {
   const gameSession = await GameSession.findOne({ where: { enterCode } });
   if (!gameSession)
@@ -17,11 +21,11 @@ const findGameSession = async (enterCode) => {
 const service = async ({ enterCode, name }) => {
   const gameSession = await findGameSession(enterCode)
 
-  const player = await Player.findOne({ where: { gameSessionId: gameSession.id, name } });
+  const player = await Player.findOne({ where: { gameSessionId: gameSession.id, name }, include: includes });
   if (player && player.id) 
     return await player.update({ updatedAt: new Date() });
   else
-    return await Player.create({ name, gameSessionId: gameSession.id, createdAt: new Date(), updatedAt: new Date() });
+    return await Player.create({ name, gameSessionId: gameSession.id, createdAt: new Date(), updatedAt: new Date() }, { include: includes });
 };
 
 export default service;
