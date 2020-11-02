38097d5d03122fed645c22f791c8cdadade61b9c
Jakub Flasiński <jk.flasinski@gmail.com>
2020-11-02 21:32:29 +0100

    change app/server to server/index

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import http from 'http';
+import app from './server';
+import setupWebsocket from './config/websocket';
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+export const websocket = setupWebsocket(server);
+
+server.listen(port, () => console.log(`DooDoop listening on port ${port}!`))
--- src/server.js
+++ src/server.js
@@ -1,11 +1,42 @@
-import http from 'http';
-import app from './app';
-import setupWebsocket from './config/websocket';
+import cors from 'cors';
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import './config/environment';
+import graphql from './config/graphql';
+import models from './models';
+import handleErrors from './middleware/handleErrors';
+import broadcastEvent from './utils/broadcastEvent';
+import { UnprocessableEntity } from './utils/errors';
 
-const port = process.env.PORT || 3000;
-app.set('port', port);
+const app = express();
 
-const server = http.createServer(app);
-export const websocket = setupWebsocket(server);
+const whiteList = [process.env.FRONTEND_URL || 'http://localhost:8080'];
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
 
-server.listen(port, () => console.log(`DooDoop listening on port ${port}!`))
+app.use(bodyParser.json());
+app.use(cors(corsOptions));
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(
+  morgan('@-- :date[web] -- METHOD_ :method -- URL_ :url -- STATUS_ :status -- RESPONSE_ :response-time[digits]ms')
+);
+app.use('/graphql', graphql);
+app.get('/game_sessions', (req, res) => {
+  broadcastEvent("test", { dupa: 'xd' });
+  models.GameSession.findAll().then((gs) => res.end(JSON.stringify(gs)))
+});
+app.use(handleErrors)
+console.log('testapp')
+export default app;
