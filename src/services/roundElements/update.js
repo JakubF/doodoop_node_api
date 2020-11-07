3ec5728f227691461cfb414e070b32e432442a6a
Jakub Flasiński <jk.flasinski@gmail.com>
2020-11-07 17:09:46 +0100

    change round element update to use new shared function

--- src/services/roundElements/update.js
+++ src/services/roundElements/update.js
@@ -2,16 +2,7 @@ import { RoundElement, GameSession } from '../../models';
 import validateSize from '../validations/validateSize';
 import validateUniqueness from '../validations/validateUniqueness';
 import validatePresence from '../validations/validatePresence';
-import { NotFound, UnprocessableEntity } from '../../utils/errors';
-
-const findRoundElement = async (id) => {
-  const record = await RoundElement.findOne({ where: { id }, include: [{ model: GameSession, required: false, as: 'gameSession' }] })
-  if (!record)
-    throw new NotFound(`Record with id ${id} not found.`)
-  if (record.gameSession.status !== 'pending')
-    throw new UnprocessableEntity('Only pending games can edited');
-  return record
-}
+import findRoundElement from './utils/findRoundElement';
 
 const validateInput = async (record, params) => {
   const attributes = { updatedAt: new Date() };
